48d6a4062e062705b22f0088ff62de365c02509d
Danil Treskov <treskovdanilv@gmail.com>
2023-05-08 22:13:10 +0500

    Register Ingredient model on Admin.

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from models import Ingredient
 
-# Register your models here.
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'measurement_unit',)
+    search_fields = ('name', 'measurement_unit',)
+    list_filter = ('name', 'measurement_unit',)
+    empty_value_display = '-пусто-'
