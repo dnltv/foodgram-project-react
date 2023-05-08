883a528ef2f4bddd208498714f6b32ef51063312
Danil Treskov <treskovdanilv@gmail.com>
2023-05-08 22:10:21 +0500

    Create Ingredient model.

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Ingredient(models.Model):
+    """A model representing an Ingredient for a Receipt."""
+    name = models.CharField(max_length=200, verbose_name='Name', required=True)
+    measurement_unit = models.CharField(max_length=200, verbose_name='Unit of measurement')
+
+    class Meta:
+        verbose_name = 'Ingredient'
+        verbose_name_plural = 'Ingredients'
+
+    def __str__(self):
+        return f'{self.name} ({self.measurement_unit})'
