c90184d2774316ae4ce08144126007ed1fae665b
Danil Treskov <treskovdanilv@gmail.com>
2023-05-11 20:50:25 +0500

    Create Recipe model.

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,8 +1,14 @@
+from typing import Optional
+
+from django.contrib.auth import get_user_model
 from django.db import models
 
 
+User = get_user_model()
+
+
 class Ingredient(models.Model):
-    """A model representing an Ingredient for a Receipt."""
+    """A model representing an Ingredient for a Recipe."""
     name = models.CharField(max_length=200, verbose_name='Name')
     measurement_unit = models.CharField(
         max_length=200,
@@ -15,3 +21,37 @@ class Ingredient(models.Model):
 
     def __str__(self):
         return f'{self.name} ({self.measurement_unit})'
+
+
+class Recipe(models.Model):
+    """A model representing a recipe."""
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='recipes',
+        verbose_name='Автор'
+    )
+    name = models.CharField(max_length=200, verbose_name='Name of the recipe')
+    text = models.TextField(verbose_name='Text')
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        through='RecipeIngredient',
+        through_fields=('recipe', 'ingredient'),
+        verbose_name='Ingredients'
+    )
+    slug = models.SlugField(verbose_name='slug')
+    pub_date = models.DateTimeField(
+        verbose_name='Date of publication',
+        auto_now=True,
+        db_index=True
+    )
+    objects = 'RecipeQuerySet'.as_manager()
+
+    class Meta:
+        ordering = ('-pub_date', )
+        verbose_name = 'Recipe'
+        verbose_name_plural = 'Recipes'
+
+    def __str__(self):
+        return self.name
+
