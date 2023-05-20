8ad379ed558b0f4db074504d441784b54150983c
Danil Treskov <treskovdanilv@gmail.com>
2023-05-20 16:20:28 +0500

    Create router for api.

---
+++ backend/api/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from django.urls import include
+from rest_framework.routers import DefaultRouter
+
+from api.views import RecipesViewSet
+
+
+router_v1 = DefaultRouter()
+router_v1.register(r'recipes', RecipesViewSet)
+
+urlpatterns = [
+    url(r'^auth/', include('djoser.urls.authtoken')),
+    url(r'', include(router_v1.urls)),
+]
