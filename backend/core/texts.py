de02b573b89fd6d28fa03fe2340cdf65dc298a91
Danil Treskov <treskovdanilv@gmail.com>
2023-05-25 20:31:00 +0500

    Add all validators for the project.

--- backend/core/texts.py
+++ backend/core/texts.py
@@ -7,13 +7,13 @@ USERS_HELP_EMAIL = (
     f'Максимум {Limits.MAX_LEN_EMAIL_FIELD} букв.'
 )
 # help-text для username
-USERS_HELP_UNAME = (
+USERS_HELP_USERNAME = (
     'Обязательно для заполнения. '
     f'От {Limits.MIN_LEN_USERNAME} до {Limits.MAX_LEN_USERS_CHARFIELD} букв.'
 )
 
 # help-text для first_name/last_name
-USERS_HELP_FNAME = (
+USERS_HELP_FIRSTNAME = (
     'Обязательно для заполнения. '
     f'Максимум {Limits.MAX_LEN_USERS_CHARFIELD} букв.'
 )
