095a3d2b51276c108e1473bec43d6552db47ec0e
Danil Treskov <treskovdanilv@gmail.com>
2023-05-28 01:47:44 +0500

    Create permissions for api.

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -1,67 +1,14 @@
-from django.core.handlers.wsgi import WSGIRequest
-from django.db.models import Model
-from rest_framework.permissions import DjangoModelPermissions  # noqa F401
-from rest_framework.permissions import IsAuthenticated  # noqa F401
-from rest_framework.permissions import SAFE_METHODS, BasePermission
-from rest_framework.routers import APIRootView
+from rest_framework import permissions
 
 
-class BanPermission(BasePermission):
-    def has_permission(
-        self,
-        request: WSGIRequest,
-        view: APIRootView
-    ) -> bool:
-        return bool(
-            request.method in SAFE_METHODS
-            or request.user.is_authenticated
-            and request.user.is_active
-        )
+class AdminOwnerReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method == 'POST':
+            return request.user.is_authenticated
+        return True
 
-
-class AdminOrReadOnly(BanPermission):
-    def has_object_permission(
-        self,
-        request: WSGIRequest,
-        view: APIRootView
-    ) -> bool:
-        return (
-            request.method in SAFE_METHODS
-            or request.user.is_authenticated
-            and request.user.is_active
-            and request.user.is_staff
-        )
-
-
-class OwnerUserOrReadOnly(BanPermission):
-    def has_object_permission(
-        self,
-        request: WSGIRequest,
-        view: APIRootView,
-        obj: Model
-    ) -> bool:
-        return (
-            request.method in SAFE_METHODS
-            or request.user.is_authenticated
-            and request.user.is_active
-            and request.user == obj.author
-            or request.user.is_staff
-        )
-
-
-class AuthorStaffOrReadOnly(BanPermission):
-    def has_object_permission(
-        self,
-        request: WSGIRequest,
-        view: APIRootView,
-        obj: Model
-    ) -> bool:
-        return (
-            request.method in SAFE_METHODS
-            or request.user.is_authenticated
-            and request.user.is_active
-            and (
-                request.user == obj.author
-                or request.user.is_staff
-            )
-        )
+    def has_object_permission(self, request, view, obj):
+        return (request.method in permissions.SAFE_METHODS
+                or obj.author == request.user
+                or request.user.is_moderator
+                or request.user.is_admin)
