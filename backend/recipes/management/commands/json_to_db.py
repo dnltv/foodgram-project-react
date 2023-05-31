7c3e3779108e4f89c44542b7f1be6cdc0e0caf23
Danil Treskov <treskovdanilv@gmail.com>
2023-05-31 12:44:00 +0500

    Edit management commands for filled database. Config docker-compose. Add data to recipe app.

--- backend/recipes/management/commands/json_to_db.py
+++ backend/recipes/management/commands/json_to_db.py
@@ -3,6 +3,7 @@ import json
 from django.core.management.base import BaseCommand
 
 from recipes.models import Ingredient, Tag
+from users.models import User
 
 
 class Command(BaseCommand):
@@ -17,19 +18,23 @@ class Command(BaseCommand):
             jsondata = json.load(f)
             if 'color' in jsondata[0]:
                 for line in jsondata:
-                    if not Tag.objects.get_or_create(
-                       slug=line['slug']).exists():
-                        Tag.objects.create(
-                            name=line['name'],
-                            color=line['color'],
-                            slug=line['slug'],
-                        )
+                    Tag.objects.create(
+                        name=line['name'],
+                        color=line['color'],
+                        slug=line['slug'],
+                    )
             elif 'measurement_unit' in jsondata[0]:
                 for line in jsondata:
-                    if not Ingredient.objects.get_or_create(
-                       name=line['name'],
-                       measurement_unit=line['measurement_unit']).exists():
-                        Ingredient.objects.create(
-                            name=line['name'],
-                            measurement_unit=line['measurement_unit']
-                        )
+                    Ingredient.objects.create(
+                        name=line['name'],
+                        measurement_unit=line['measurement_unit']
+                    )
+            elif 'email' in jsondata[0]:
+                for line in jsondata:
+                    User.objects.create(
+                        email=line['email'],
+                        username=line['username'],
+                        first_name=line['first_name'],
+                        last_name=line['last_name'],
+                        password=line['password'],
+                    )
